34a8888d5358f2af7ed870f1b5bdef5f7b08ad00
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-23 16:47:24 +0200

    added integration test for cleanNumbers() in numbers.test.js

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -1,6 +1,6 @@
-import { it, expect, describe } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
-import { transformToNumber } from './numbers';
+import { cleanNumbers, transformToNumber } from './numbers';
 
 describe('transformToNumber()', () => {
   it('should transform a string number to a number of type number', () => {
@@ -29,4 +29,14 @@ describe('transformToNumber()', () => {
     expect(result).toBeNaN();
     expect(result2).toBeNaN();
   });  
+});
+
+describe('cleanNumbers()', () => {
+  it('should return an array of number values if an array of string number values is provided', () => {
+    const numberValues = ['1', '2'];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
+  });
 });
